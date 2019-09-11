93b754cbd6b9cd602f3cda8aea177c3a4b480215
lernen 2019 <littlein1227>
2019-09-11 21:26:51 +0200

    test 1

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,19 @@
+package com.tom
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello()
+      val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+//        println("Hello kotlin")
+//
+        Person p = new Person();
+    }int age ;
+    age = 22;
+
+}
+
